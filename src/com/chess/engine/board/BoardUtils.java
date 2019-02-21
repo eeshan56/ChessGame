cfb58b86436de03b9f07abd8b721543dc8fdb528
eeshan <eeshanpanse@gmail.com>
2019-02-22 00:54:15 +0530

    Initial commit

---
+++ src/com/chess/engine/board/BoardUtils.java
@@ -0,0 +1,36 @@
+package com.chess.engine.board;
+
+public class BoardUtils {
+
+    public static boolean[] FIRST_COLUMN = initColumn(0);
+    public static boolean[] SECOND_COLUMN = initColumn(1);
+    public static boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final int NUM_TILES = 64;
+    public static final int NUM_TILES_PER_ROW = 8;
+
+    private static boolean[] initColumn(int columnNumber) {
+
+        final boolean column[] = new boolean [NUM_TILES];
+
+        do {
+
+            column[columnNumber] = true;
+            columnNumber += NUM_TILES_PER_ROW;
+
+        }while (columnNumber < NUM_TILES);
+
+        return column;
+    }
+
+    private BoardUtils() {
+        throw new RuntimeException("You cannot instantiate me!");
+    }
+
+    public static boolean isValidTileCoordinate(int candidateDestinationCoordinate) {
+
+        return candidateDestinationCoordinate >= 0 && candidateDestinationCoordinate < NUM_TILES;
+    }
+
+}
