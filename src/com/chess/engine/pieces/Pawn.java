e9306fc92981aa21416e4f4ac3bc9ae35b1ec4f5
eeshan <eeshanpanse@gmail.com>
2019-02-26 01:14:36 +0530

    Pawn beta 1

---
+++ src/com/chess/engine/pieces/Pawn.java
@@ -0,0 +1,45 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Pawn extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTORS = {8};
+
+    Pawn(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+
+        final List <Move> legalMoves = new ArrayList <> ();
+
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_VECTORS) {
+
+            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+
+            if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                continue;
+            }
+
+            if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                //TODO auto generated stub
+
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+            }
+
+            
+
+        }
+
+        return legalMoves;
+    }
+}
